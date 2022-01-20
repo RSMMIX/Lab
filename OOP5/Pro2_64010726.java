63b2847da096dd6597e5fc77963b6394775c49dd
Rattanaporn <88606135+RSMMIX@users.noreply.github.com>
2022-01-21 02:24:59 +0700

    Lab

--- OOP5/Pro2_64010726.java
+++ OOP5/Pro2_64010726.java
@@ -1,6 +1,8 @@
 public class Pro2_64010726 {
     public static void main(String[] args) {
-        
+        // RegularPolygon mypol = new RegularPolygon(6, 4);
+        // System.out.println("The area of a regular polygon is");
+        // System.out.println("The area of a regular polygon is");
     }
 }
 
@@ -11,11 +13,25 @@ class RegularPolygon {
     private double y; // y coordinate
 
     // pubilc RegularPolygon(int n, double side) {
-    //     this.n = n;
-    //     this.side = side;
-    //     x = 0;
-    //     y = 0;
+    // this.n = n;
+    // this.side = side;
+    // x = 0;
+    // y = 0;
     // }
+
+    RegularPolygon(int n, double side) {
+        this.n = n;
+        this.side = side;
+        x = 0;
+        y = 0;
+    }
+
+    RegularPolygon(int n, double side , double x,double y){
+        this.n = n;
+        this.side = side;
+        this.x = x;
+        this.y = y;
+    }
 
     public int getN() {
         return n;
