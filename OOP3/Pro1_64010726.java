2fb4066794d32517ed317034e805e9b84c595c4e
Rattanaporn <88606135+RSMMIX@users.noreply.github.com>
2022-01-24 16:31:51 +0700

    lab

--- OOP3/Pro1_64010726.java
+++ OOP3/Pro1_64010726.java
@@ -2,7 +2,7 @@ public class Pro1_64010726 {
 
     static boolean isPrime(int n){
         if(n < 2) return false;
-        for (int i = 2; i*i < n; i++) {
+        for (int i = 2; i*i <= n; i++) {
             if (n % i == 0) return false;
         }
         return true;
