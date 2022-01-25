49eb70602402a7d9664f0ab4e110cfb3b19ce501
Rattanaporn <88606135+RSMMIX@users.noreply.github.com>
2022-01-25 14:59:54 +0700

    lab

--- OOP5/Pro2_64010726.java
+++ OOP5/Pro2_64010726.java
@@ -1,32 +1,33 @@
 public class Pro2_64010726 {
     public static void main(String[] args) {
-        // RegularPolygon mypol = new RegularPolygon(6, 4);
-        // System.out.println("The area of a regular polygon is");
-        // System.out.println("The area of a regular polygon is");
+        RegularPolygon mypol = new RegularPolygon();
+        RegularPolygon polmy = new RegularPolygon(6, 4);
+        RegularPolygon olmyp = new RegularPolygon(10 , 4, 5.6, 7.8);
+        System.out.println("The perimeter of mypol is "+mypol.getPerimeter());
+        System.out.println("The area of mypol is "+mypol.getArea());
+        System.out.println("The perimeter of polmy is "+polmy.getPerimeter());
+        System.out.println("The area of polmy is "+polmy.getArea());
+        System.out.println("The perimeter of olmyp is "+olmyp.getPerimeter());
+        System.out.println("The area of olmyp is "+olmyp.getArea());
     }
 }
 
 class RegularPolygon {
-    private int n; // number of sides of the polygon
-    private double side;
-    private double x; // x coordinate
-    private double y; // y coordinate
+    private int n = 3; // number of sides of the polygon
+    private double side = 1;
+    private double x = 0; // x coordinate
+    private double y = 0; // y coordinate
 
-    // pubilc RegularPolygon(int n, double side) {
-    // this.n = n;
-    // this.side = side;
-    // x = 0;
-    // y = 0;
-    // }
+    public RegularPolygon() {
 
-    RegularPolygon(int n, double side) {
-        this.n = n;
-        this.side = side;
-        x = 0;
-        y = 0;
     }
 
-    RegularPolygon(int n, double side , double x,double y){
+    public RegularPolygon(int n, double side) {
+        this.n = n;
+        this.side = side;
+    }
+
+    public RegularPolygon(int n, double side, double x, double y) {
         this.n = n;
         this.side = side;
         this.x = x;
@@ -54,7 +55,22 @@ class RegularPolygon {
     }
 
     public double getArea() {
-        return (n * side) * 5;
+        return (n * side * side) / (4 * Math.tan(Math.PI / n));
     }
 
+    public void setN(int n) {
+        this.n = n;
+    }
+
+    public void setSide(Double side) {
+        this.side = side;
+    }
+
+    public void setX(Double x) {
+        this.x = x;
+    }
+
+    public void setY(Double y) {
+        this.y = y;
+    }
 }
