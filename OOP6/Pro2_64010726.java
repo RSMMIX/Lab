a2583ec74cdcf7e18ae47b4faeae0f17a2eb45f7
Rattanaporn <88606135+RSMMIX@users.noreply.github.com>
2022-01-31 17:51:40 +0700

    lab

--- OOP6/Pro2_64010726.java
+++ OOP6/Pro2_64010726.java
@@ -1,3 +1,18 @@
+import java.util.*;
+
 public class Pro2_64010726 {
-    
+
+    public static void main(String[] args) {
+
+    }
+
+    class Course {
+        private String name;
+        private String[] students;
+        private int sumStudents;
+
+        public void dropStudent(){
+
+        }
+    }
 }
