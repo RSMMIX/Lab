87e27b32fa2bf195ba9a457cae946ad94f35af9c
Rattanaporn <88606135+RSMMIX@users.noreply.github.com>
2022-02-04 20:10:08 +0700

    lab

--- OOP6/Pro2_64010726.java
+++ OOP6/Pro2_64010726.java
@@ -1,38 +1,60 @@
-import java.io.StreamCorruptedException;
-import java.util.*;
-
 public class Pro2_64010726 {
 
     public static void main(String[] args) {
-
+        Course course = new Course("Prince");
+        course.addStudent("Moji");
+        course.addStudent("Pawn");
+        course.addStudent("Molinchi");
+        course.dropStudent("Pawn");
+        for (int i = 0; i < course.getNumberOfStudents(); i++) {
+            System.out.println(course.getStudents()[i]);
+        }
     }
 
-    class Course {
-        private String courseName;
-        private String[] students = new String[100];
-        private int numberOfStudents;
+}
+class Course {
+    private String courseName;
+    private String[] students = new String[2];
+    private int numberOfStudents;
 
-        public Course(String courString){
-            this.courseName = courseName;
-        }
-        public void addStudent() {
-            students[numberOfStudents] = students;
-            numberOfStudents++;
-        }
-        public String[] getStudents() {
-            return students;
-        }
+    public Course(String courseName) {
+        this.courseName = courseName;
+    }
 
-        public int getNumberOfStudents(){
-            return numberOfStudents;
+    public void addStudent(String student) {
+        if (numberOfStudents >= students.length) {
+            String[] temp = new String[students.length * 2];
+            System.arraycopy(students, 0, temp, 0, students.length);
+            students = temp;
         }
+        students[numberOfStudents++] = student;
+    }
 
-        public String getCourseString(){
-            return courseName;
+    public String[] getStudents() {
+        return students;
+    }
+
+    public int getNumberOfStudents() {
+        return numberOfStudents;
+    }
+
+    public String getCourseString() {
+        return courseName;
+    }
+
+    public void dropStudent(String student) {
+        for (int i = 0; i < numberOfStudents; i++) {
+            if (students[i].equals(student)) {
+                System.arraycopy(students, i + 1, students, i, numberOfStudents - i - 1);
+                students[numberOfStudents - i] = null;
+                break;
+            }
         }
+        numberOfStudents--;
+    }
 
-        public void dropStudent(){
-
-        }
+    public void clear() {
+        students = new String[2];
+        numberOfStudents = 0;
     }
 }
--- OOP6/Pro3_64010726.java
+++ OOP6/Pro3_64010726.java
@@ -1,35 +1,48 @@
 public class Pro3_64010726 {
     public static void main(String[] args) {
         Queue queue = new Queue();
-        
-        for (int i = 0; i < 20; i++) {
-             queue.enqueue(i);
+
+        for (int i = 1; i <= 20; i++) {
+            queue.enqueue(i);
+        }
+        while (!queue.empty()) {
+            System.out.println(queue.dequeue());
         }
     }
+}
 
-    class Queue {
-        private int[] elements;
-        private int size;
-        private boolean empty;
+class Queue {
+    private int[] elements;
+    private int size;
 
-        public Queue() {
-            elements = new int[8];
-        }
-
-        public boolean empty(){
-            return size == 0;
-        }
-
-        public int getSize(){
-            return size;
-        }
-
-        public void enqueue(int v) {
-
-        }
-
-        public int dequeue() {
-
-        }
+    public Queue() {
+        elements = new int[8];
     }
+
+    public void enqueue(int v) {
+        if (size >= elements.length) {
+            int[] temp = new int[elements.length * 2];
+            System.arraycopy(elements, 0, temp, 0, elements.length);
+            elements = temp;
+        }
+        elements[size++] = v;
+    }
+
+    public int dequeue() {
+        if (empty()) {
+            return 0;
+        }
+        int result = elements[0];
+        System.arraycopy(elements, 1, elements, 0, --size);
+        return result;
+    }
+
+    public boolean empty() {
+        return size == 0;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
 }
